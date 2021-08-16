1d0a462bf36f8118fc0a58eeafa6de18e642e1a9
thiagocessin <thiagoacessin@gmail.com>
2021-08-16 19:25:52 -0300

    Liberando cors para acesso local

--- src/app.js
+++ src/app.js
@@ -5,10 +5,18 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin","http://localhost:8100");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, Content-Type, Accept"
+    );
+
+    next();
+});
+
 app.get("/cursos", listarTodos);
 app.get("/cursos/:cursoId", buscarCurso);
-
-
 app.post("/cursos",store);
 
 
